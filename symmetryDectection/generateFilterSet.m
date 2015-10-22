90e8a4809b1b4b8eecb7430b8e44f5fb147a766c
Mads <madsclund@gmail.com>
2015-10-21 22:54:48 -0400

    Generate filter set makes the image a square

--- symmetryDectection/generateFilterSet.m
+++ symmetryDectection/generateFilterSet.m
@@ -17,46 +17,21 @@ for i = 1:length(sigmas)
         % Padd
         imgPad = padarray(img,[xPad yPad],'replicate');
         imgPadFilterd = conv2(imgPad, kernel, 'same');
-        filterSet.filtered{i,j} = imgPadFilterd(xPad+1:end-xPad, yPad+1:end-yPad);
+        filtered = imgPadFilterd(xPad+1:end-xPad, yPad+1:end-yPad);
+
+        % Use only central square of image
+        [v, minIndx] = min(size(filtered));
+        d = abs(diff(size(filtered)));
+        if minIndx == 1
+            filtered(:,1:floor(d/2)) = 0;
+            filtered(:,(end-ceil(d/2)):end) = 0;
+        else
+            filtered(1:floor(d/2),:) = 0;
+            filtered((end-floor(d/2)):end,:) = 0;
+        end
         
-%         if visualize >= 1
-%             fig = figure(42);
-%             
-%             subplot(2, length(filterAngles),j)
-%             imagesc(imag(filterSet.filtered{i,j}))
-%             [px, py] = pol2cart(filterAngles(j),20);
-%             line(xPad/2+[0, px], yPad/2 + [0, py])
-%             title(filterAngles(j))
-%             
-%             subplot(2,length(filterAngles),j+length(filterAngles))
-%             imagesc(real(filterSet.filtered{i,j}))
-%             [px, py] = pol2cart(filterAngles(j),20);
-%             line(xPad/2+[0, px], yPad/2 + [0, py])
-%             title(filterAngles(j))
-%             
-            
-%             drawnow
-%             subplot(2,2,1)
-%             imagesc(imag(kernel))
-%             [px, py] = pol2cart(filterAngles(j),20);
-%             line(xPad/2+[0, px], yPad/2 + [0, py])
-%             title(filterAngles(j))
-%             subplot(2,2,2)
-%             imagesc(imag(filterSet.filtered{i,j}))
-%             [px, py] = pol2cart(filterAngles(j),20);
-%             line(xPad/2+[0, px], yPad/2 + [0, py])
-%             title(filterAngles(j))
-%             subplot(2,2,3)
-%             imagesc(real(kernel))
-%             line(xPad/2+[0, px], yPad/2 + [0, py])
-%             title(filterAngles(j))
-%             subplot(2,2,4)
-%             imagesc(real(filterSet.filtered{i,j}))
-%             [px, py] = pol2cart(filterAngles(j),20);
-%             line(xPad/2+[0, px], yPad/2 + [0, py])
-%             title(filterAngles(j))
-%             drawnow
-%         end
+        filterSet.filtered{i,j} = filtered;
+        
         
     end
 end
